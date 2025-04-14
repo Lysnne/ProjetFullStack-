0ec55bb719fd46eaaa728228825cfe15c2b7f0fb
Elverovski <MarioEnriqueSilvaBenavides2020@gmail.com>
2025-04-14 17:46:24 -0400

    Market done

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -1,7 +1,7 @@
 import 'react';
 import axios from 'axios';
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import {Link, useParams } from "react-router-dom";
 
 const API_URL = import.meta.env.VITE_API_URL || ""
 
@@ -11,27 +11,36 @@ function Market() {
 
 
     useEffect(() => {
-        loadStock();
+        loadStock("AllStocks");
     }, []);
 
 
-    const loadStock = async () => {
-        const result = await axios.get(`${API_URL}/stock/getAllStocks`)
-        console.log("Data received:", result.data)
-        setStocks(result.data);
+    const loadStock = async (dataType) => {
+        try {
+
+            const url = `${API_URL}/stock/get${dataType}`
+            const result = await axios.get(url)
+            console.log("Data received:", result.data)
+            setStocks(result.data);
+        }
+        catch (error) {
+            console.error("Eror loading the stocks:", error)
+        }
     };
+
 
 
     return (
         <div className="container mt-4">
             <h1>Market</h1>
 
+            <h4>Sort By: </h4>
             <div className="d-flex gap-2 mb-3">
-                <button className="btn btn-primary">Top</button>
-                <button className="btn btn-primary">Trending</button>
-                <button className="btn btn-primary">Gainers</button>
-                <button className="btn btn-primary">Losers</button>
-                <button className="btn btn-primary">New</button>
+
+                <button onClick={() => loadStock("AllStocksSortedByPrice")} className="btn btn-primary">Price</button> 
+                <button onClick={() => loadStock("AllStocksSortedByName")} className="btn btn-primary">Name</button>
+                <button onClick={() => loadStock("AllStocksSortedByVolume")} className="btn btn-primary">Volume</button>
+                <button onClick={() => loadStock("AllStocksSortedByMarketCap")} className="btn btn-primary">Market Cap</button>
 
             </div>
 
@@ -39,9 +48,10 @@ function Market() {
                 <div className="card-body">
                     <div className="row fw-bold text-uppercase border-bottom pb-2">
                         <div className="col-1">#</div>
-                        <div className="col-3">Name</div>
-                        <div className="col-2">Symbol</div>
-                        <div className="col-2">Price</div>
+                        <div className="col-2">Name</div>
+                        <div className="col-1">Symbol</div>
+                        <div className="col-1">Price</div>
+                        <div className="col-3">Sector</div>
                         <div className="col-2">Volume</div>
                         <div className="col-2">Market Cap</div>
                     </div>
@@ -49,10 +59,11 @@ function Market() {
                     {stocks.map((stock, index) => (
                         <div key={stock.symbol} className="row py-2 border-bottom align-items-center">
                             <div className="col-1">{index + 1}</div>
-                            <div className="col-3">{stock.name}</div>
-                            <div className="col-2">{stock.symbol}</div>
-                            <div className="col-2">${stock.price}</div>
-                            <div className="col-2">{stock.sector}</div>
+                            <div className="col-2">{stock.name}</div>
+                            <div className="col-1">{stock.symbol}</div>
+                            <div className="col-1">${stock.price}</div>
+                            <div className="col-3">{stock.sector}</div>
+                            <div className="col-2">{stock.volume}</div>
                             <div className="col-2">{stock.marketcap}</div>
                         </div>
                     ))}
