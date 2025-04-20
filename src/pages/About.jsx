7fdb82d5bd73ce0716e4f2880c718b52e712570d
ikasaidi <ikramsaidi47@gmail.com>
2025-04-20 19:56:55 -0400

    tools fini, et changement de text pour about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import '../styles/AboutUs.css';
 function About() {
     return (
         <div className="container py-5">
-        <h1 className="about-title text-center mb-5">About us</h1>
+        <h1 className="about-title text-center mb-5">À propos d'Assetra</h1>
         <div className="row align-items-center">
           <div className="col-md-6 mb-4 mb-md-0">
             <img
@@ -14,9 +14,11 @@ function About() {
           </div>
           <div className="col-md-6">
             <p className="about-text">
-              In the heart of a bustling city, where skyscrapers cast long shadows over busy streets, life unfolds in a vibrant tapestry of cultures, sounds, and stories. Each corner brims with the aroma of diverse cuisines, beckoning passersby to explore new flavors, while street performers share their talents, creating a symphony of music that dances through the air.
-              
-              Amidst the frenetic energy, moments of serenity can be found in tranquil parks, where individuals escape the chaos, finding solace in nature's embrace. This dynamic interplay of urban life reflects not just a place but a profound celebration of humanity's endless pursuit of connection, creativity, and experience.
+            Dans un monde où les finances jouent un rôle central dans nos vies, il est crucial de les comprendre pour mieux bâtir son avenir. C’est pour cette raison qu’Assetra a vu le jour : une plateforme conçue pour aider les utilisateurs, même sans connaissances financières, à prendre des décisions éclairées.
+
+            Notre mission est simple : démocratiser l’accès à l’investissement et à l’éducation financière, grâce à des outils intuitifs, des simulateurs performants et une interface accessible à tous. Que ce soit pour calculer ses impôts, simuler un placement ou comprendre le marché des actions, Assetra guide chaque utilisateur pas à pas.
+
+            Sur notre site, chaque fonctionnalité a été pensée pour vous accompagner dans votre parcours financier, avec clarté, simplicité et sécurité. Parce que gérer son argent ne devrait jamais être une source de stress, mais un levier pour grandir.
             </p>
           </div>
         </div>
